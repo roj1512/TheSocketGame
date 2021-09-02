d88bb91729b31a5c07382dbdeeb4a974e195c1b1
Roj Serbest <roj@rojser.best>
2021-09-02 19:18:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# TheSocketGame
+
+Note: this is not ws, it is raw TCP.
+
+## Running
+
+```bash
+go run .
+```
+
+## Rules
+
+- Send `scores\n` to get all scores in JSON format.
+- Send `+[yourname]\n` to increase your score.
+
+## Credits
+
+- Original idea from [ShiSHcat](https://github.com/ShiSHcat).
+- Rewritten by [me](https://github.com/rojserbest).
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+var scores = map[string]int64{}
+
+func main() {
+	address := os.Getenv("HOST")
+	if address == "" {
+		address = "0.0.0.0:3444"
+	}
+
+	server, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go listen(server)
+	idle()
+}
+
+func listen(server net.Listener) {
+	for {
+		conn, err := server.Accept()
+		go handle(conn, err)
+	}
+}
+
+func handle(conn net.Conn, err error) error {
+	if err != nil {
+		return err
+	}
+
+	message, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		return err
+	}
+	
+	message = strings.TrimRight(message, "\n")
+
+	if strings.HasPrefix(message, "+") {
+		user := strings.TrimLeft(message, "+")
+		if user == "" {
+			return conn.Close()
+		}
+
+		scores[user] = scores[user] + 1
+	} else {
+		err := json.NewEncoder(conn).Encode(scores)
+		if (err != nil) {
+			return err
+		}
+	}
+
+	return conn.Close()
+}
+
+func idle() {
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
